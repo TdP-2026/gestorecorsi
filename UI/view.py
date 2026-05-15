2705ed79205588efc33a24f12ef4f8c2425c8611
fedyn0 <federicoparisi03@gmail.com>
2026-05-15 12:28:26 +0200

    Dealing with DAO

--- UI/view.py
+++ UI/view.py
@@ -6,36 +6,54 @@ class View(ft.UserControl):
         super().__init__()
         # page stuff
         self._page = page
-        self._page.title = "Template application using MVC and DAO"
+        self._page.title = "Gestore corsi - Edizione 2026"
         self._page.horizontal_alignment = 'CENTER'
-        self._page.theme_mode = ft.ThemeMode.DARK
+        self._page.theme_mode = ft.ThemeMode.LIGHT
         # controller (it is not initialized. Must be initialized in the main, after the controller is created)
         self._controller = None
         # graphical elements
         self._title = None
-        self.txt_name = None
-        self.btn_hello = None
-        self.txt_result = None
-        self.txt_container = None
+
+        self._ddPD = None
+        self._ddCodIns = None
+        self._btnPrintCorsiPD = None
+        self._btnPrintIscrittiCorsiPD = None
+        self._btnPrintIscrittiCodins = None
+        self._btnPrintCDSCodins = None
+
+
 
     def load_interface(self):
         # title
-        self._title = ft.Text("Hello World", color="blue", size=24)
+        self._title = ft.Text("Gestore corsi - Edizione 2026", color="blue", size=24)
         self._page.controls.append(self._title)
 
-        #ROW with some controls
-        # text field for the name
-        self.txt_name = ft.TextField(
-            label="name",
-            width=200,
-            hint_text="Insert a your name"
-        )
+        # ROW 1
+        self._ddPD = ft.Dropdown(label = "Periodo Didattico",
+                                 options = [ft.dropdown.Option("I"), ft.dropdown.Option("II")],
+                                 width= 200)
+        self._btnPrintCorsiPD = ft.ElevatedButton(text = "Stampa corsi",
+                                                  on_click= self._controller.handlePrintCorsiPD,
+                                                  width= 300)
+        self._btnPrintIscrittiCorsiPD = ft.ElevatedButton(text = "Stampa numero iscritti",
+                                                          on_click= self._controller.handlePrintIscrittiCorsiPD,
+                                                          width= 300)
 
-        # button for the "hello" reply
-        self.btn_hello = ft.ElevatedButton(text="Hello", on_click=self._controller.handle_hello)
-        row1 = ft.Row([self.txt_name, self.btn_hello],
-                      alignment=ft.MainAxisAlignment.CENTER)
-        self._page.controls.append(row1)
+        row1 = ft.Row([self._ddPD, self._btnPrintCorsiPD, self._btnPrintIscrittiCorsiPD])
+
+        self._ddCodIns = ft.Dropdown(label = "Corso",
+                                     width= 200)
+
+        self._controller.fillddCodins()
+        self._btnPrintIscrittiCodins = ft.ElevatedButton(text = "Stampa iscritti al corso ",
+                                                         on_click= self._controller.handlePrintIscrittiCodins,
+                                                         width= 300)
+        self._btnPrintCDSCodins = ft.ElevatedButton(text = "Stampa CDS afferenti",
+                                                    on_click= self._controller.handlePrintCDSCodins,
+                                                    width= 300)
+
+        row2 = ft.Row([self._ddCodIns, self._btnPrintIscrittiCodins,self._btnPrintCDSCodins])
+        self._page.add(row1, row2)
 
         # List View where the reply is printed
         self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
--- database/DAO.py
+++ database/DAO.py
@@ -2,5 +2,21 @@ from database.DB_connect import DBConnect
 
 
 class DAO():
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def getCodins(self):
+        cnx = DBConnect.get_connection()
+        cursor = cnx.cursor(dictionary=True)
+
+        query = """
+        """
+
+        cursor.execute(query)
+
+        res = []
+        for row in cursor:
+            pass
+
+        cursor.close()
+        cnx.close()
+        return res
